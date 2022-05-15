abe69bb8b058ad9981004412a7443b275d4a939c
bsamu <bsamuel0220@gmail.com>
2022-05-16 00:28:23 +0200

    property binding done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StringInterpolationServerComponent } from './string-interpolation-server/string-interpolation-server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StringInterpolationServerComponent
+    StringInterpolationServerComponent,
+    ServersComponent,
   ],
   imports: [
     BrowserModule
